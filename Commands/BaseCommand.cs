235b52c41989fc34f612c201b97cc9249b93f5f7
Javid <52424037+javidhasanzade@users.noreply.github.com>
2020-11-11 23:54:24 +0400

    Add files via upload

---
+++ Commands/BaseCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace BeautySalon.Commands
+{
+    class BaseCommand : ICommand
+    {
+        private Action<object> action;
+        private Func<bool> canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public BaseCommand(Action<object> action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)=> canExecute?.Invoke() ?? true;
+       
+        public void Execute(object parameter) => action?.Invoke(parameter);
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}

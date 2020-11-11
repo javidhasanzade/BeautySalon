994f903d9a2e3d0a5f3693081e2daf5fe853e6f1
Javid <52424037+javidhasanzade@users.noreply.github.com>
2020-11-11 23:53:19 +0400

    Added Services folder

---
+++ Services/BeautySalonDBContext.cs
@@ -0,0 +1,231 @@
+﻿using BeautySalon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeautySalon.Services
+{
+    public partial class BeautySalonDBContext : DbContext
+    {
+        public BeautySalonDBContext()
+        {
+        }
+
+        public BeautySalonDBContext(DbContextOptions<BeautySalonDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Appointment> Appointments { get; set; }
+        public virtual DbSet<Branch> Branches { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Earning> Earnings { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Expense> Expenses { get; set; }
+        public virtual DbSet<LaserEarning> LaserEarnings { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<Service> Services { get; set; }
+        public virtual DbSet<StaffEarning> StaffEarnings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=62.171.143.1; Database=BeautySalon;Integrated Security=False; User=SA; Password=HUk465cUmRneMAHbN3b3JmQzFjkZ7adz"); // "Server=(localdb)\\mssqllocaldb;Database=BeautySalonDB;Trusted_Connection=True;"
+            }
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>(entity =>
+            {
+                entity.Property(e => e.Checkout).HasColumnType("datetime");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.Notes).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.Appointments)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Appointme__Branc__51300E55");
+
+                entity.HasOne(d => d.Client)
+                    .WithMany(p => p.Appointments)
+                    .HasForeignKey(d => d.ClientId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Appointme__Clien__5224328E");
+
+                entity.HasOne(d => d.Service)
+                    .WithMany(p => p.Appointments)
+                    .HasForeignKey(d => d.ServiceId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Appointme__Servi__531856C7");
+
+                entity.HasOne(d => d.Staff)
+                    .WithMany(p => p.Appointments)
+                    .HasForeignKey(d => d.StaffId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Appointme__Staff__540C7B00");
+            });
+
+            modelBuilder.Entity<Branch>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(40);
+            });
+
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.PhoneNumber)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.Clients)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Clients__BranchI__5441852A");
+            });
+
+            modelBuilder.Entity<Earning>(entity =>
+            {
+                entity.Property(e => e.Amount).HasColumnType("money");
+
+                entity.HasOne(d => d.Appointment)
+                    .WithMany(p => p.Earnings)
+                    .HasForeignKey(d => d.AppointmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Earnings__Appoin__56E8E7AB");
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.Earnings)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Earnings__Branch__57DD0BE4");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Employees__Branc__398D8EEE");
+
+                entity.HasOne(d => d.Position)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.PositionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Employees__Posit__38996AB5");
+            });
+
+            modelBuilder.Entity<Expense>(entity =>
+            {
+                entity.Property(e => e.Amount).HasColumnType("money");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Time)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.Expenses)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Expenses__Branch__5BE2A6F2");
+            });
+
+            modelBuilder.Entity<LaserEarning>(entity =>
+            {
+                entity.Property(e => e.Amount).HasColumnType("money");
+
+                entity.HasOne(d => d.Appointment)
+                    .WithMany(p => p.LaserEarnings)
+                    .HasForeignKey(d => d.AppointmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__LaserEarn__Appoi__634EBE90");
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.LaserEarnings)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__LaserEarn__Branc__6442E2C9");
+            });
+
+            modelBuilder.Entity<Position>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Service>(entity =>
+            {
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<StaffEarning>(entity =>
+            {
+                entity.Property(e => e.Amount).HasColumnType("money");
+
+                entity.HasOne(d => d.Appointment)
+                    .WithMany(p => p.StaffEarnings)
+                    .HasForeignKey(d => d.AppointmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__StaffEarn__Appoi__5AB9788F");
+
+                entity.HasOne(d => d.Branch)
+                    .WithMany(p => p.StaffEarnings)
+                    .HasForeignKey(d => d.BranchId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__StaffEarn__Branc__5BAD9CC8");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.StaffEarnings)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__StaffEarn__Emplo__5CA1C101");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
